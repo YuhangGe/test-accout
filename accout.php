9e639aa8ba62f3f9b4da46afbfe279c8dde61c8c
Yuhang Ge <abeyuhang@gmail.com>
2011-08-11 12:13:18 +0800

    some change

--- accout.php
+++ accout.php
@@ -1,38 +1,25 @@
 <?php
 require "function.php";
 
-if(empty($_COOKIE['test_accouts'])){
-	echo 'var TEST_ACCOUTS=[];';
-	exit();
-}
-if(!preg_match('/^\d+(,\d+)*$/',$_COOKIE['test_accouts'])){
-	echo 'var TEST_ACCOUTS=[];';
-	exit();
-}
-
-$call_back=null;
+$call_back = null;
 if(!empty($_REQUEST['callback']))
-	$callback=$_REQUEST['callback'];
+	$callback = $_REQUEST['callback'];
 
-
-$coo_accouts=explode(',', $_COOKIE['test_accouts']);
-
-
-
-require "db.php";
-
-$output=array();
-
-foreach($coo_accouts as $id){
-	$accout=getAccout($id);
-	foreach($accout as $ea)
-		$output[]=$ea;
+$output = array();
+if(!empty($_COOKIE['test_accouts']) && preg_match('/^\d+(,\d+)*$/', $_COOKIE['test_accouts'])) {
+	$coo_accouts = explode(',', $_COOKIE['test_accouts']);
+	require "db.php";
+	foreach($coo_accouts as $id) {
+		$accout = getAccout($id);
+		foreach($accout as $ea)
+			$output[] = $ea;
+	}
 }
 
-if($callback==null)
-	echo 'var TEST_ACCOUTS='.json_encode($output).";";
+if($callback == null)
+	echo 'var TEST_ACCOUTS=' . json_encode($output) . ";";
 else
-	echo $callback.'('.json_encode($output).');';
+	echo $callback . '(' . json_encode($output) . ');';
 
 mysql_close($db_connection);
 ?>
